fc3dda41ae8226e9de5fb0f550bb819b5f986117
ajikurniawan076 <ajikurniawan076@gmail.com>
2025-02-19 10:25:13 +0700

    a

--- admin/src/ProtectedRoutes.jsx
+++ admin/src/ProtectedRoutes.jsx
@@ -6,15 +6,15 @@ export const ProtectedRoutes = () => {
 
     if (!token) return <Navigate to="../" replace />;
 
-    try {
-        const decoded = jwtDecode(token);
-        if (decoded.exp * 1000 < Date.now()) {
-            localStorage.removeItem("token"); // Remove expired token
-            return <Navigate to="../" replace />;
-        }
-    } catch (error) {
-        return <Navigate to="../" replace />;
-    }
+    // try {
+    //     const decoded = jwtDecode(token);
+    //     if (decoded.exp * 1000 < Date.now()) {
+    //         localStorage.removeItem("token"); // Remove expired token
+    //         return <Navigate to="../" replace />;
+    //     }
+    // } catch (error) {
+    //     return <Navigate to="../" replace />;
+    // }
 
     return <Outlet />;
 }
