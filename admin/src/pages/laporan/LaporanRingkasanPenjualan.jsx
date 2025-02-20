eb8f370bc55cdcd80ea7c5c82d241d4c5c6177fa
rezasetyab <rezasetya09@gmail.com>
2025-02-20 11:48:50 +0700

    reza

--- admin/src/pages/laporan/LaporanRingkasanPenjualan.jsx
+++ admin/src/pages/laporan/LaporanRingkasanPenjualan.jsx
@@ -12,7 +12,7 @@ import i3 from "../../assets/iconLaporanPenjualan/i3.svg";
 import i4 from "../../assets/iconLaporanPenjualan/i4.svg";
 import DatePicker from 'react-datepicker'
 import axios from 'axios';
-import { toDate } from 'date-fns';
+import { startOfWeek, toDate } from 'date-fns';
 import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from "recharts";
@@ -28,15 +28,7 @@ export const LaporanRingkasanPenjualan = () => {
     const [endDate, setEndDate] = useState(new Date().toISOString().split('.')[0] + 'Z');
     const [data, setData] = useState();
     const navigate = useNavigate();
-    const [chartData, setChartData] = ([
-        { name: "Senin", penjualan: 0 },
-        { name: "Selasa", penjualan: 0 },
-        { name: "Rabu", penjualan: 0 },
-        { name: "Kamis", penjualan: 0 },
-        { name: "Jumat", penjualan: 0 },
-        { name: "Sabtu", penjualan: 0 },
-        { name: "Minggu", penjualan: 0 },
-    ])
+    const [chartData, setChartData] = useState([])
 
     const handleNavigate = (e) => {
         e.preventDefault()
@@ -100,17 +92,21 @@ export const LaporanRingkasanPenjualan = () => {
         console.log(tanggal)
     }, [startDate, endDate])
 
-    // useEffect(() => {
-    //     const fetchChart = async () => {
-    //         try {
-    //             const response = await axios.get("https://api.drnich.co.id/api/pos/laporan/laporangrafik")
-    //             setChartData(response.data)
-    //         } catch (error) {
-    //             console.log("Error Saat Fetching Chart data:", error)
-    //         }
-    //     }
-    //     fetchChart()
-    // },[])
+    useEffect(() => {
+        const tanggal = {
+            endOfWeek : (new Date().toISOString().split('.')[0] + 'Z')
+        }
+        const fetchChart = async () => {
+            try {
+                const response = await axios.post("https://api.drnich.co.id/api/pos/laporan/laporangrafik", tanggal)
+                setChartData(response.data.transactions)
+                console.log(response.data.transactions)
+            } catch (error) {
+                console.log("Error Saat Fetching Chart data:", error)
+            }
+        }
+        fetchChart()
+    },[])
 
 
     setLink('/pos/laporan')
@@ -253,35 +249,35 @@ export const LaporanRingkasanPenjualan = () => {
                 <div className="text-[12px] bg-[#F6F6F6] text-[#BDBDBD] text-start my-[17px] w-full">
                     <p>Grafik Penjualan</p>
                 </div>
-                <div style={{ width: '100%', height: 300 }}>
-                <ResponsiveContainer width="100%" height={300}>
+                <div style={{ width: '100%', height: 400 }}>
+                <ResponsiveContainer width="100%" height={400}>
                     <BarChart
                     data={chartData}
                     margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
                     >
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
+                    <XAxis dataKey="name" tick={{ fontSize: 12 }} angle={-40} textAnchor="end" />
                     <YAxis tickFormatter={(val) => {
-                        if (val >= 1000000) return `${val/1000000}jt`
-                        return val
+                        if (val >= 1000000) return `${(val/1000000).toLocaleString("id-ID")}jt`
+                        return val.toLocaleString('id-ID')
                     }} />
                     <Tooltip formatter={(value) => new Intl.NumberFormat('id-ID', {
                         style: 'currency',
                         currency: 'IDR'
                     }).format(value)} />
                     <Legend />
-                    <Bar
-                        dataKey="penjualan"
-                        fill="url(#colorGradient)"
-                        radius={[5, 5, 0, 0]}
-                    >
+                            <Bar
+                                dataKey="penjualan"
+                                fill="url(#colorGradient)"
+                                radius={[5, 5, 0, 0]}
+                            >
+                            </Bar>
                         <defs>
-                        <linearGradient id="colorGradient" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="0%" stopColor="#FFC107" stopOpacity={0.9}/>
-                            <stop offset="100%" stopColor="#FF8A00" stopOpacity={0.7}/>
-                        </linearGradient>
+                            <linearGradient id="colorGradient" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="0%" stopColor="#FFC120" stopOpacity={0.9}/>
+                                <stop offset="100%" stopColor="#F8A39B" stopOpacity={0.7}/>
+                            </linearGradient>
                         </defs>
-                    </Bar>
                     </BarChart>
                 </ResponsiveContainer>
                 </div>
@@ -314,116 +310,6 @@ export const LaporanRingkasanPenjualan = () => {
                         </div>
                     </div>
                 </div>
-
-
-                {/* <div className="text-[12px] bg-[#F6F6F6] text-[#BDBDBD] text-start w-full">
-            <p className="">Produk Terlaris</p>
-        </div>
-        <div className='grid mt-3'>
-            <div className='flex h-full gap-3'>
-                <div className='flex items-center text-center gap-8 border rounded-xl text-[12px] text-[#454545] border-[#BDBDBD] p-1 px-4 w-fit h-[130%]'>
-                    <p>Banyak Produk terjual</p>        
-                    <img src={iPanahB} alt="iPanahB" className='w-[20px] h-[20px]' />
-                </div>
-                <div className='flex items-center text-center gap-16 border rounded-xl text-[12px] text-[#454545] border-[#BDBDBD] p-1 px-4 w-fit h-[130%]'>
-                    <p>Jasa</p>        
-                    <img src={iPanahB} alt="iPanahB" className='w-[20px] h-[20px]' />
-                </div>
-            </div>
-        </div>
-        <div className='grid'>
-            <div className='flex justify-between p-4 border border-[#BDBDBD] rounded-xl mt-7 mb-1 text-[12px]'>
-                <div className='flex items-center text-center gap-3'>
-                    <img src={i1} alt="" />
-                    <p>Facial Glow Acne</p>
-                </div>
-                <div className='text-[#C2A353]'>
-                    <p>5000 terjual</p>
-                </div>
-            </div>
-            <div className='flex justify-between p-4 border border-[#BDBDBD] rounded-xl my-1 text-[12px]'>
-                <div className='flex items-center text-center gap-3'>
-                    <img src={i2} alt="" />
-                    <p>Facial Gold</p>
-                </div>
-                <div className='text-[#C2A353]'>
-                    <p>5000 terjual</p>
-                </div>
-            </div>
-            <div className='flex justify-between p-4 border border-[#BDBDBD] rounded-xl my-1 text-[12px]'>
-                <div className='flex items-center text-center gap-3'>
-                    <img src={i3} alt="" />
-                    <p>Laser</p>
-                </div>
-                <div className='text-[#C2A353]'>
-                    <p>5000 terjual</p>
-                </div>
-            </div>
-            <div className='flex justify-between p-4 border border-[#BDBDBD] rounded-xl my-1 text-[12px]'>
-                <div className='flex items-center text-center gap-3'>
-                    <img src={i4} alt="" />
-                    <p>Facial Glow Acne</p>
-                </div>
-                <div className='text-[#C2A353]'>
-                    <p>5000 terjual</p>
-                </div>
-            </div>
-        </div> */}
-
-
-                {/* <div className="text-[12px] bg-[#F6F6F6] text-[#BDBDBD] text-start w-full mt-2">
-            <p className="">Kategori Teratas</p>
-        </div>
-        <div className='grid mt-3'>
-            <div className='flex h-full gap-3'>
-                <div className='flex items-center text-center gap-6 border rounded-xl text-[12px] text-[#454545] border-[#BDBDBD] p-1 px-4 w-fit h-[130%]'>
-                    <p>Pendapatan Penjualan</p>        
-                    <img src={iPanahB} alt="iPanahB" className='w-[20px] h-[20px]' />
-                </div>
-                <div className='flex items-center text-center gap-16 border rounded-xl text-[12px] text-[#454545] border-[#BDBDBD] p-1 px-4 w-fit h-[130%]'>
-                    <p>Semua</p>        
-                    <img src={iPanahB} alt="iPanahB" className='w-[20px] h-[20px]' />
-                </div>
-            </div>
-        </div>
-        <div className='grid'>
-            <div className='flex justify-between p-4 border border-[#BDBDBD] rounded-xl mt-7 mb-1 text-[12px]'>
-                <div className='flex items-center text-center gap-3'>
-                    <img src={i1} alt="" />
-                    <p>Facial Series</p>
-                </div>
-                <div className='text-[#C2A353]'>
-                    <p>Rp 200.000.000</p>
-                </div>
-            </div>
-            <div className='flex justify-between p-4 border border-[#BDBDBD] rounded-xl my-1 text-[12px]'>
-                <div className='flex items-center text-center gap-3'>
-                    <img src={i2} alt="" />
-                    <p>Sunscreen</p>
-                </div>
-                <div className='text-[#C2A353]'>
-                    <p>Rp 120.000.000</p>
-                </div>
-            </div>
-            <div className='flex justify-between p-4 border border-[#BDBDBD] rounded-xl my-1 text-[12px]'>
-                <div className='flex items-center text-center gap-3'>
-                    <img src={i3} alt="" />
-                    <p>Serum</p>
-                </div>
-                <div className='text-[#C2A353]'>
-                    <p>Rp 50.000.000</p>
-                </div>
-            </div>
-            <div className='flex justify-between p-4 border border-[#BDBDBD] rounded-xl my-1 text-[12px]'>
-                <div className='flex items-center text-center gap-3'>
-                    <img src={i4} alt="" />
-                    <p>Toner</p>
-                </div>
-                <div className='text-[#C2A353]'>
-                    <p>Rp. 10.000.000</p>
-                </div>
-            </div>
-        </div> */}
 
                 <div className="text-[12px] bg-[#F6F6F6] text-[#BDBDBD] text-start my-[17px] w-full">
                     <p className="">Pelanggan Teratas</p>
